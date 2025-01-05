16686027714368968ceb405261fcccae98d1c874
Louie Brooks <louiewbrooks04@gmail.com>
2025-01-05 17:53:07 +0000

    going to instead use a struct so i don't need to pass in many values

--- staticNN.c
+++ staticNN.c
@@ -3,13 +3,17 @@
 
 void initialiseWeights() {
     double x[2][1] = {{3}, {4}};
-    double W1[2][2];
-    double B1[2];
-    double W2[2];
-    double B2[2];
+    double w1[2][2] = {{0.7, 0.8}, {-0.2, 0.5}};
+    double b1[2] = {{0.5}, {0.7}};
+    double w2[2] = {{0.1}, {0.1}};
+    double b2[1] = {0.3};
+    double learningRate = 0.1;
     
     printf("X values:\n");
     printArray(2, 1, (double *)x);
+    printf("W1 values:\n");
+    printArray(2, 2, (double *)w1);
+
 }
 
 void staticNN() {
--- util.c
+++ util.c
@@ -3,9 +3,14 @@
 void printArray(int rows, int cols, double *arr) {
     for (int i = 0; i < rows; i ++) {
         for (int j = 0; j < cols; j++) {
-            printf("%f\t", *(arr + i * cols + j)); // increments the pointer by the amount of memory that a double takes up
-                                                 // in dynamic memory allocation this must be done manually
+            printf("%f\t", *(arr + i * cols + j)); 
+            // increments the pointer by the amount of memory that a double takes up
+            // in dynamic memory allocation this must be done manually
         }
         printf("\n");
     }
 }
+
+// double[] dotProduct(int r1, int c1, double *arr1, int r2, int c2, double *arr2) {
+// 
+// }
